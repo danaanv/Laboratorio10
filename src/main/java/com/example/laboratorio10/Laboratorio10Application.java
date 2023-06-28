aea68952cc528bedeae6795d98f4b58f153ffa9d
danaanv <a20202330@pucp.edu.pe>
2023-06-27 19:06:40 -0500

    Initial commit

---
+++ src/main/java/com/example/laboratorio10/Laboratorio10Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/laboratorio10/Laboratorio10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
